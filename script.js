1b3804a71d28e0f43c8e1b0cea537a3e74cf6db8
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2024-12-03 01:58:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -73,16 +73,11 @@ const eventData = {
     ]
   };
   
-function basket1Price() {
-    for (let i = 0; i < eventData.ticketTypes.length; i++) {
-      if (eventData.ticketTypes[i].name === "Basket 1") {
-        return eventData.ticketTypes[i].price;
-      }
-    }
-    return "Ticket not found";
-  }
+function firstBasketPrice() {
+  tkt.ticketTypes[0].price
+}
+console.log("First Basket Ticket Price:", firstBasketPrice());
   
-  console.log("Price of Basket 1 Ticket:", basket1Price());
 
 function eventStartDate () {
     eventData.startsAtUtc;
